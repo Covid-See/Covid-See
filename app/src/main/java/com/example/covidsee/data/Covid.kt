b345c83592b2ded80d5af95ce02f02c7828b7dd3
ZzukMa <67806878+minhyuuk@users.noreply.github.com>
2021-06-28 11:41:49 +0900

    [ADD] data class

---
+++ app/src/main/java/com/example/covidsee/data/Covid.kt
@@ -0,0 +1,25 @@
+package com.example.covidsee.data
+
+data class Covid(
+    val busan: Busan,
+    val chungbuk: Chungbuk,
+    val chungnam: Chungnam,
+    val daegu: Daegu,
+    val daejeon: Daejeon,
+    val gangwon: Gangwon,
+    val gwangju: Gwangju,
+    val gyeongbuk: Gyeongbuk,
+    val gyeonggi: Gyeonggi,
+    val gyeongnam: Gyeongnam,
+    val incheon: Incheon,
+    val jeju: Jeju,
+    val jeonbuk: Jeonbuk,
+    val jeonnam: Jeonnam,
+    val korea: Korea,
+    val quarantine: Quarantine,
+    val resultCode: String,
+    val resultMessage: String,
+    val sejong: Sejong,
+    val seoul: Seoul,
+    val ulsan: Ulsan
+)
