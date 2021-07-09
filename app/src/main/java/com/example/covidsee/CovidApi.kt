1e4ce209ca7c383241c93cf03ac33820538c5cb7
ZzukMa <67806878+minhyuuk@users.noreply.github.com>
2021-07-09 12:23:55 +0900

    [ADD] token

--- app/src/main/java/com/example/covidsee/CovidApi.kt
+++ app/src/main/java/com/example/covidsee/CovidApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 class CovidApi {
     companion object {
         val DOMAIN = "https://api.corona-19.kr"
-        val TOKEN = "token 값 작성"
+        val TOKEN = "EDdKRf7gXjQCOyi9mpH85uFrZlbWTzkAe"
     }
 }
 
